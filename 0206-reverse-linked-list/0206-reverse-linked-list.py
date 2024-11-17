957aabb88c361648994f83895abc6cab641526eb
Aman Singh <116139327+amancd@users.noreply.github.com>
2024-11-18 00:48:26 +0530

    Time: 3 ms (7.62%), Space: 17.6 MB (73.94%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,17 +1,12 @@
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        # Initialize pointers
-        prev = None  # Previous node starts as None
-        curr = head  # Current node starts at the head
+        prev = None
+        curr = head
 
-        while curr is not None:
-            next_node = curr.next
-            
+        while curr:
+            temp = curr.next
             curr.next = prev
-            
-
             prev = curr
-            curr = next_node
-
-        # prev is now the new head of the reversed list
+            curr = temp
+        
         return prev
