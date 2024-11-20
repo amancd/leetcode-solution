4d382011eca1cfe7ce752a995eb55973d1d4fd5a
Aman Singh <116139327+amancd@users.noreply.github.com>
2024-11-20 17:23:41 +0530

    Time: 342 ms (24.44%), Space: 17.2 MB (55.21%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
@@ -1,25 +1,22 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        total_count = Counter(s)
-        if total_count['a'] < k or total_count['b'] < k or total_count['c'] < k:
+        total = Counter(s)
+        if total['a'] < k or total['b'] < k or total['c'] < k:
             return -1
         
         n = len(s)
-        left_count = Counter({'a': 0, 'b': 0, 'c': 0})
-        max_valid_length = 0
-        start = 0
+        count = Counter({'a':0, 'b':0, 'c':0})
+        length = 0
+        left = 0
 
-        for end in range(n):
-            left_count[s[end]] += 1
+        for right in range(n):
+            count[s[right]] += 1
 
-            while (
-                total_count['a'] - left_count['a'] < k or
-                total_count['b'] - left_count['b'] < k or
-                total_count['c'] - left_count['c'] < k
-            ):
-                left_count[s[start]] -= 1
-                start += 1
+            while( total['a'] - count['a'] < k or total['b'] - count['b'] < k or total['c'] - count['c'] < k):
+                count[s[left]] -= 1
+                left += 1
 
-            max_valid_length = max(max_valid_length, end - start + 1)
+            
+            length = max(length, right-left + 1)
 
-        return n - max_valid_length
+        return n - length
