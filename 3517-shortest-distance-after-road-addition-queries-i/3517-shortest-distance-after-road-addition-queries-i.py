3b3d32887fb686770cebc4e0ac487ac36f78b5ae
Aman Singh <116139327+amancd@users.noreply.github.com>
2024-11-27 21:52:28 +0530

    Time: 249 ms (90.98%), Space: 17.6 MB (8.31%) - LeetHub

---
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        
+        graph = defaultdict(list)
+        for i in range(n - 1):
+            graph[i].append(i + 1)
+
+        def bfs(start, target):
+            queue = deque([start])
+            distances = [-1] * n
+            distances[start] = 0
+
+            while queue:
+                node = queue.popleft()
+                for neighbor in graph[node]:
+                    if distances[neighbor] == -1:  # not visited
+                        distances[neighbor] = distances[node] + 1
+                        queue.append(neighbor)
+                        if neighbor == target:
+                            return distances[neighbor]
+            return -1 
+
+        result = []
+        for ui, vi in queries:
+            graph[ui].append(vi)
+            shortest = bfs(0, n - 1)
+            result.append(shortest)
+
+        return result
