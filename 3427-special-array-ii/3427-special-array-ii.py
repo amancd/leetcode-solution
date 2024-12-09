113a8f79dc305e3e072654831c89d3330b5a767b
Aman Singh <116139327+amancd@users.noreply.github.com>
2024-12-09 10:07:33 +0530

    Time: 39 ms (77.78%), Space: 46.6 MB (53.04%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.py
+++ 3427-special-array-ii/3427-special-array-ii.py
@@ -5,11 +5,10 @@ class Solution:
 
         for i in range(1, n):
             prefix[i] = prefix[i - 1]
-            if (nums[i - 1] % 2 == 0 and nums[i] % 2 == 0) or (nums[i - 1] % 2 != 0 and nums[i] % 2 != 0):
+            if nums[i - 1] % 2 == nums[i] % 2:
                 prefix[i] += 1
 
         result = []
-
 
         for left, right in queries:
             special_count = prefix[right] - (prefix[left] if left > 0 else 0)
