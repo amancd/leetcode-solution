fd39d5700aa0417b6467cd94f04e765e142b1991
Aman Singh <116139327+amancd@users.noreply.github.com>
2025-04-02 23:20:25 +0530

    Time: 296 ms (5.24%), Space: 17.6 MB (96.26%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.py
@@ -1,15 +1,13 @@
-from typing import List
-
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         
         def fn(i, j, k):
-            return max((i - j) * k, 0)  # Ensures non-negative result
+            return max((i - j) * k, 0)
         
         maximum = 0
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):  # j > i
-                for k in range(j + 1, len(nums)):  # k > j
-                    maximum = max(maximum, fn(nums[i], nums[j], nums[k]))  # Fix: Move this inside k loop
+            for j in range(i + 1, len(nums)): 
+                for k in range(j + 1, len(nums)): 
+                    maximum = max(maximum, fn(nums[i], nums[j], nums[k]))
 
         return maximum
