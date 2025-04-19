997c68d7e15ac74d91a44059261da424b55c0da4
Aman Singh <116139327+amancd@users.noreply.github.com>
2025-04-19 22:31:14 +0530

    Time: 1061 ms (13.46%), Space: 31.6 MB (53.88%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -1,18 +1,51 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        
+        #Optimal
         nums.sort()
         n = len(nums)
         count = 0
-        for i in range(n):
+
+        # Upper bound
+        for i in range(n-1):
             l = i + 1
             r = n - 1
+            while l<=r:
+                mid = (l + r)//2
 
-            while l <= r and nums[l] + nums[i] < lower:
-                l += 1
+                if nums[i] + nums[mid] >=lower:
+                    r = mid - 1
+                
+                else:
+                    l = mid + 1
+            
+            lower_bound = l
 
-            while r >= l and nums[r] + nums[i] > upper:
-                r -= 1
+        # Lower bound
+            l = i + 1
+            r = n - 1
+            while l<=r:
+                mid = (l + r)//2
 
-            count += max(0, r - l + 1)
+                if nums[i] + nums[mid] <=upper:
+                    l = mid + 1
+                
+                else:
+                    r = mid - 1
+            
+            upper_bound = r
 
+            if lower_bound <= upper_bound:
+                count += upper_bound - lower_bound + 1
+        
         return count
+
+        # Brute 
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if i>=0 and i<j<len(nums):
+        #             if lower <= (nums[i] + nums[j]) <= upper:
+        #                 count+=1
+        
+        # return count
