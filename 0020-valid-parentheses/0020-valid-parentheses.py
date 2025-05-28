66bce6239876a7995b89361a3859d0bfb9729a8a
Aman Singh <116139327+amancd@users.noreply.github.com>
2025-05-28 22:57:27 +0530

    Time: 0 ms (100%), Space: 18 MB (10.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,12 +7,12 @@ class Solution:
         for st in s:
             if st not in pairs:
                 stack.append(st)
-        
-
+            
             else:
                 if not stack or stack[-1] != pairs[st]:
                     return False
+
                 else:
                     stack.pop()
-            
+        
         return True if not stack else False
