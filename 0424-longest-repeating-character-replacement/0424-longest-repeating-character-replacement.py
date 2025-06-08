cc6c7bf301e8c9226974e6536a7fd2be1cb4e644
Aman Singh <116139327+amancd@users.noreply.github.com>
2025-06-08 19:50:05 +0530

    Time: 116 ms (67.13%), Space: 18.2 MB (23.42%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,5 +1,6 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+
         freq = {}
 
         max_count = 0
@@ -14,8 +15,7 @@ class Solution:
             if (right - left + 1) - max_count > k:
                 freq[s[left]] -= 1
                 left += 1
-
+            
             max_length = max(max_length, right - left + 1)
         
         return max_length
-             
