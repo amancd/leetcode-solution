dc1ed37f744391aaa22b2d44a9db2e6f24aba7b1
Aman Singh <116139327+amancd@users.noreply.github.com>
2025-06-24 08:54:12 +0530

    Time: 9692 ms (5%), Space: 17.7 MB (83.7%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -7,8 +7,8 @@ class Solution:
         for i in range(len(s)):
             for j in range(i+1):
                 if s[j:i+1] == s[j:i+1][::-1]:
-                    if len(s[j:i+1]) > maximum:
-                        res = s[j:i+1]
+                    if maximum < len(s[j:i+1]):
                         maximum = len(s[j:i+1])
+                        res = s[j:i+1]
         
         return res
