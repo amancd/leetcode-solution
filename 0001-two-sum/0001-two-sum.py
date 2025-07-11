8b83a8177b803ed930b52c1277ba17f58b362703
Aman Singh <116139327+amancd@users.noreply.github.com>
2025-07-11 09:34:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (10.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,14 +2,14 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         comp = {}
-
+        
         for i in range(len(nums)):
+
             diff = target - nums[i]
 
             if nums[i] in comp:
                 return [comp[nums[i]], i]
             
-            
             comp[diff] = i
-        
+
         return []
